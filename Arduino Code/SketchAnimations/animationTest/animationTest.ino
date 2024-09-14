f54294f2f15e812cf1afc672b0dbf6197b0e0cb9
scuyul <krima724@gmail.com>
2024-09-14 11:40:03 -0500

    fe

--- Arduino Code/SketchAnimations/animationTest/animationTest.ino
+++ Arduino Code/SketchAnimations/animationTest/animationTest.ino
@@ -11,6 +11,12 @@ void setup() {
   Serial.begin(9600);
 }
 
+
+
+
+
+
+
 void loop() {
   //If the RoboRIO has sent something
   if(Serial.available()) {
@@ -19,23 +25,18 @@ void loop() {
 
     //If the byte is 0x12 (i.e. 18 in decimal)
     if(value == 0x12) {
-
-      delay(500);  
-  leds[1] = CRGB(0, 255, 0);
-  FastLED.show();
-  delay(500);
-  leds[2] = CRGB(255, 0, 255);
-  FastLED.show();
-  delay(500);
-
       //Write a string to the output buffer as a response to send to the RoboRIO
-     
-     
-     
-    }
-  }
 
-  //Delay 50 milliseconds, otherwise the loop method runs as fast as possible over and over again which isn't really necessary and can sometimes cause
-  //instability in the callbacks used to manage the serial buffers when using higher baud rates. 
+     delay(500);  
+  leds[1] = CRGB(255, 255, 255);
+  FastLED.show();
+  delay(500);
+  leds[1] = CRGB(0, 0, 0);
+  FastLED.show();
+  delay(500);
+     
+     
+    };
+  };
   delay(50);
-}
+};
